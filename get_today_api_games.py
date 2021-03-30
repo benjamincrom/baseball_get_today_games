e3ed29926b76b4be5e950e17c9a534b80f133f04
benjamincrom@gmail.com <benjamincrom@gmail.com>
2021-03-30 14:07:26 -0400

    Coming right along

--- get_today_api_games.py
+++ get_today_api_games.py
@@ -185,6 +185,8 @@ HTML_INDEX_PAGE = (
             '<option value="31">31</option>'
           '</select>'
           '<select name="year" id="year">'
+            '<option value="2021">2021</option>'
+            '<option value="2020">2020</option>'
             '<option value="2019">2019</option>'
             '<option value="2018">2018</option>'
             '<option value="2017">2017</option>'
