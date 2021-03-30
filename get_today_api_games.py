93d66df47b65aff8c4c76af0b38b4ca765090644
benjamincrom@gmail.com <benjamincrom@gmail.com>
2021-03-30 16:07:59 -0400

    Coming right along

--- get_today_api_games.py
+++ get_today_api_games.py
@@ -820,7 +820,6 @@ def write_games_for_date(this_datetime, output_dir):
             baseball.fetch_game.write_game_svg_and_html(game.game_date_str, game, output_dir)
             if len(game.game_date_str.split('-')) == 6:
                 game_html_id_list.append(game.game_date_str)
-                import pdb; pdb.set_trace()
         except Exception as e:
             print(game_dict['gameData']['game']['id'])
             print(e)
