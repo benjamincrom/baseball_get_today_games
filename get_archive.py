43978db2ff25485726eddd31ee1968d45a83471b
benjamincrom@gmail.com <benjamincrom@gmail.com>
2021-04-09 12:31:12 -0400

    Coming right along

--- get_archive.py
+++ get_archive.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from sys import exc_info
 from traceback import format_exception
 
+import requests
 import wget
 
 MLB_TEAM_CODE_DICT = {'LAA': 'ana',
@@ -37,57 +38,114 @@ MLB_TEAM_CODE_DICT = {'LAA': 'ana',
                       'TB':  'tba',
                       'BOS': 'bos'}
 
-MLB_URL_PATTERN = ('http://gd2.mlb.com/components/game/mlb/year_{year}/'
-                   'month_{month}/day_{day}/gid_{year}_{month}_{day}_'
-                   '{away_mlb_code}mlb_{home_mlb_code}mlb_{game_number}/')
+ALL_GAMES_URL = ('http://gdx.mlb.com/components/game/mlb/year_{year:02d}/'
+                 'month_{month:02d}/day_{day:02d}/miniscoreboard.json')
+
+MLB_URL_PATTERN = ('http://gd2.mlb.com/components/game/mlb/year_{year:04d}/'
+                   'month_{month:02d}/day_{day:02d}/gid_{game_id}/')
+
+GAME_URL_2020_TEMPLATE = ('http://statsapi.mlb.com/api/v1.1/game/{game_pk}'
+                          '/feed/live')
 
 BOXSCORE_SUFFIX = 'boxscore.xml'
 PLAYERS_SUFFIX = 'players.xml'
 INNING_SUFFIX = 'inning/inning_all.xml'
 
-THIS_YEAR = 2018
+def get_archive_2019(this_date, filehandle):
+    month = this_date.month
+    day = this_date.day
+    year = this_date.year
+    try:
+        all_games_dict = requests.get(
+            ALL_GAMES_URL.format(month=month, day=day, year=year)
+        ).json()
 
-with open('file_list.txt') as fh:
-    rough_name_list = fh.readlines()
+        game_id_list = [
+            game_dict['id'].replace('-', '_').replace('/', '_')
+            for game_dict in all_games_dict['data']['games'].get('game', [])
+        ]
+    except:
+        exc_type, exc_value, exc_traceback = exc_info()
+        lines = format_exception(exc_type, exc_value, exc_traceback)
+        exception_str = ' '.join(lines)
+        filehandle.write('{} {}\n\n'.format(str(this_date),
+                                            exception_str))
 
-name_list = [x.strip()[:-4] for x in rough_name_list
-             if len(x.strip()) > 15 and x.strip()[-4:] != 'html']
+        return
 
-fh = open('output_log.txt', 'w')
-this_date = datetime.datetime(2018, 1, 1, 0, 0)
-while this_date.year < 2020:
-    for name in name_list:
-        year, month, day, away_code, home_code, game_number = name.split('-')
-        if (this_date.year == int(year) and this_date.month == int(month) and
-                this_date.day == int(day)):
-            away_mlb_code = MLB_TEAM_CODE_DICT[away_code]
-            home_mlb_code = MLB_TEAM_CODE_DICT[home_code]
+    for game_id in game_id_list:
+        full_path = "{:04d}/month_{:02d}/day_{:02d}/gid_{}/".format(
+            year, month, day, game_id
+        )
 
-            dir_name = 'gid_{}_{}_{}_{}mlb_{}mlb_{}'.format(
-                year, month, day, away_mlb_code, home_mlb_code, game_number
-            )
+        Path(full_path + 'inning/').mkdir(parents=True, exist_ok=True)
 
-            full_path = "{}/month_{}/day_{}/{}/".format(
-                year, month, day, dir_name
-            )
+        url_prefix = MLB_URL_PATTERN.format(
+            year=year, month=month, day=day, game_id=game_id,
+        )
 
-            Path(full_path + 'inning/').mkdir(parents=True, exist_ok=True)
+        try:
+            wget.download(url_prefix + BOXSCORE_SUFFIX, full_path)
+            wget.download(url_prefix + PLAYERS_SUFFIX, full_path)
+            wget.download(url_prefix + INNING_SUFFIX, full_path + 'inning/')
+        except:
+            exc_type, exc_value, exc_traceback = exc_info()
+            lines = format_exception(exc_type, exc_value, exc_traceback)
+            exception_str = ' '.join(lines)
+            filehandle.write('{} {} {}\n\n'.format(str(this_date),
+                                                   url_prefix,
+                                                   exception_str))
 
-            url_prefix = MLB_URL_PATTERN.format(
-                year=year, month=month, day=day, away_mlb_code=away_mlb_code,
-                home_mlb_code=home_mlb_code, game_number=game_number
-            )
+def get_archive_2020(this_date, filehandle):
+    month = this_date.month
+    day = this_date.day
+    year = this_date.year
+    try:
+        all_games_dict = requests.get(
+            ALL_GAMES_URL.format(month=month, day=day, year=year)
+        ).json()
 
-            try:
-                wget.download(url_prefix + BOXSCORE_SUFFIX, full_path)
-                wget.download(url_prefix + PLAYERS_SUFFIX, full_path)
-                wget.download(url_prefix + INNING_SUFFIX, full_path + 'inning/')
-            except:
-                exc_type, exc_value, exc_traceback = exc_info()
-                lines = format_exception(exc_type, exc_value, exc_traceback)
-                exception_str = ' '.join(lines)
-                fh.write('{} {} {}\n\n'.format(datetime.datetime.utcnow(),
-                                               url_prefix,
-                                               exception_str))
+        game_tuple_list = [
+            (x['id'], x['game_pk'])
+            for x in all_games_dict['data']['games'].get('game', [])
+        ]
+    except:
+        exc_type, exc_value, exc_traceback = exc_info()
+        lines = format_exception(exc_type, exc_value, exc_traceback)
+        exception_str = ' '.join(lines)
+        filehandle.write('{} {}\n\n'.format(str(this_date),
+                                            exception_str))
 
-    this_date = this_date + datetime.timedelta(days=1)
+        return
+
+    for formatted_id, game_pk in game_tuple_list:
+        url = GAME_URL_2020_TEMPLATE.format(game_pk=game_pk)
+        game_id = formatted_id.replace('-', '_').replace('/', '_')
+        full_path = "{:02d}/month_{:02d}/day_{:02d}/gid_{}/".format(
+            year, month, day, game_id
+        )
+
+        Path(full_path).mkdir(parents=True, exist_ok=True)
+        try:
+            wget.download(url, full_path)
+        except:
+            exc_type, exc_value, exc_traceback = exc_info()
+            lines = format_exception(exc_type, exc_value, exc_traceback)
+            exception_str = ' '.join(lines)
+            filehandle.write('{} {} {}\n\n'.format(datetime.datetime.utcnow(),
+                                                   url,
+                                                   exception_str))
+
+if __name__ == '__main__':
+    start_date = datetime.datetime(2000, 9, 1, 0, 0)
+    end_date = datetime.datetime(2021, 4, 9, 0, 0)
+
+    fh = open('output_log.txt', 'w')
+    current_date = start_date
+    while current_date <= end_date:
+        if current_date.year < 2020:
+            get_archive_2019(current_date, fh)
+        else:
+            get_archive_2020(current_date, fh)
+
+        current_date = current_date + datetime.timedelta(days=1)
